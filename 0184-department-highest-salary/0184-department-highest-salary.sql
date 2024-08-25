67089c3fc8809e4770de09843e8d6846f33f674d
Gaurav Sahil Rodrigues <80237249+gauravrodrigues@users.noreply.github.com>
2024-08-25 16:59:34 -0400

    Time: 679 ms (40.43%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,12 +1,12 @@
 -- # Write your MySQL query statement below
 
-select 
-Department as Department, name as Employee, salary
-from
-(select d.name as Department, e.name , salary, 
+select  Department, Employee, salary
+from (
+select d.name as Department, e.name as Employee, salary, 
 dense_rank() over(partition by departmentId order by salary desc) as dns
 from Employee e left join Department d
-on e.departmentId = d.id) as dns_table
+on e.departmentId = d.id
+) as dns_table
 where dns = 1
 
 
