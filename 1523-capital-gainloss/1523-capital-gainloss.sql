d86c01f73cafffa5b9f96d5b2ebbc3d216467d86
Gaurav Sahil Rodrigues <80237249+gauravrodrigues@users.noreply.github.com>
2024-09-07 18:28:08 -0400

    Time: 634 ms (15.01%), Space: 0B (100%) - LeetHub

--- 1523-capital-gainloss/1523-capital-gainloss.sql
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -2,8 +2,7 @@
 
 
 select stock_name,
-
-sum(if(operation = "Sell", price, 0)) - sum(if(operation = "Buy", price, 0)) as capital_gain_loss
+sum(case when operation = "Buy" then price * -1 else price end) as capital_gain_loss
 from
 stocks
 group by stock_name
